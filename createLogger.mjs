a8bd89f53cef88433a0394f4a9f3a90cb7da02a8
Joel T <joel.thiessen@automattic.com>
2022-07-06 17:15:11 -0700

    Altering output for logger

--- createLogger.mjs
+++ createLogger.mjs
@@ -1,12 +1,22 @@
 export const createLogger = (str, mute = false) => ({
 	log: (...args) =>
-		mute ? () => {} : console.log(chalk.cyan.bold(str), ...args),
+		mute
+			? () => {}
+			: console.log(chalk.cyan.bold(str), chalk.white('|'), ...args),
 	warn: (...args) =>
-		mute ? () => {} : console.warn(chalk.yellow.bold(str), ...args),
+		mute
+			? () => {}
+			: console.warn(chalk.yellow.bold(str), chalk.white('|'), ...args),
 	debug: (...args) =>
-		mute ? () => {} : console.debug(chalk.gray.bold(str), ...args),
+		mute
+			? () => {}
+			: console.debug(chalk.gray.bold(str), chalk.white('|'), ...args),
 	info: (...args) =>
-		mute ? () => {} : console.info(chalk.cyan.bold(str), ...args),
+		mute
+			? () => {}
+			: console.info(chalk.cyan.bold(str), chalk.white('|'), ...args),
 	error: (...args) =>
-		mute ? () => {} : console.error(chalk.red.bold(str), ...args),
+		mute
+			? () => {}
+			: console.error(chalk.red.bold(str), chalk.white('|'), ...args),
 });
